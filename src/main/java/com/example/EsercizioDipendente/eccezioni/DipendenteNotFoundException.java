f02921faf74132d70efb8776b376485c058cb19b
Ferraria <Ferraria@host.docker.internal>
2023-10-04 17:50:22 +0200

    Commit 2

--- src/main/java/com/example/EsercizioDipendente/eccezioni/DipendenteNotFoundException.java
+++ src/main/java/com/example/EsercizioDipendente/eccezioni/DipendenteNotFoundException.java
@@ -3,7 +3,6 @@ package com.example.EsercizioDipendente.eccezioni;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@SuppressWarnings("serial")
 @ResponseStatus(HttpStatus.NOT_FOUND)
 
 public class DipendenteNotFoundException extends RuntimeException {
